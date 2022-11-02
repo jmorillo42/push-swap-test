aeb5d0a877d68e3ccbb71add51a5883ee735fa74
jmorillo <107439157+jmorillo42@users.noreply.github.com>
2022-11-02 22:37:12 +0100

    .

--- push-swap-test.py
+++ push-swap-test.py
@@ -8,7 +8,7 @@
 #    By: jmorillo <jmorillo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/16 15:14:35 by jmorillo          #+#    #+#              #
-#    Updated: 2022/11/02 15:51:15 by jmorillo         ###   ########.fr        #
+#    Updated: 2022/11/02 22:36:58 by jmorillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,20 +16,24 @@ import itertools
 import math
 import os
 import random
-import re
 import subprocess
 import sys
+import time
 
 RED = '\033[31m'
+LRED = '\033[1;31m'
 GREEN = '\033[32m'
-YELLOW = '\033[33m'
-BLUE = '\033[1;34m'
+LGREEN = '\033[1;32m'
+BLUE = '\033[34m'
+LBLUE = '\033[1;34m'
+YELLOW = '\033[1;33m'
 MAGENTA = '\033[1;35m'
 CYAN = '\033[1;36m'
 GRAY = '\033[1;30m'
+WHITE = '\033[1;37m'
 RESET = '\033[0m'
 
-#print(f'{YELLOW}Tests {RED}closed{YELLOW} for renovations{RESET}')
+#print(f'{YELLOW}Tests {LRED}closed{YELLOW} due to renovations{RESET}')
 #exit(0)
 
 COMMAND_NOT_FOUND = 'Error: Command “{}” not found'
@@ -101,89 +105,79 @@ TESTS=[
 ]
 
 def main():
-    print('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓')
-    print('┃  Tests for push-swap project  ┃')
-    print('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛')
+    print(f'{RED}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓{RESET}')
+    print(f'{RED}┃  {YELLOW}Tests for push-swap project{RESET}  {RED}┃{RESET}')
+    print(f'{RED}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛{RESET}')
     print()
+    print(f'{WHITE}INPUT/OUTPUT{RESET}')
+    print(f'{GRAY}[OK] push_swap(INPUT) = MOVEMENTS ; ERROR (RETURN_CODE){RESET}')
     print_input_output_tests()
     print()
-    print('COMBINATIONS OF N NUMBERS')
+    print(f'{WHITE}COMBINATIONS OF N NUMBERS{RESET}')
     print(' Show all')
+    print(f'  {GRAY}[OK] push_swap(INPUT) = MOVEMENTS ; ERROR (RETURN_CODE){RESET}')
     print_all_comb_numbers(3)
     #print_all_comb_numbers(4)
     #print_all_comb_numbers(5)
     print()
     print(' Only stats')
+    print(f'  {GRAY}[OK] N=# --> AVERAGE [MIN - MAX]  POINTS   AVG_TIME{RESET}')
     print_comb_numbers(2)
     print_comb_numbers(3)
     print_comb_numbers(4)
     print_comb_numbers(5)
-    #print_comb_numbers(6)
     print()
-    print('SEQUENCES OF N NUMBERS')
-    print(f' {GRAY}[OK] N=××× --> AVERAGE [MIN-MAX]{RESET}')
+    print(f'{WHITE}SEQUENCES OF N NUMBERS{RESET}')
+    print(f' {GRAY}[OK] N=### --> AVERAGE [  MIN -   MAX]  POINTS   AVG_TIME{RESET}')
+    print_sequence_numbers(50)
     print_sequence_numbers(100)
+    print_sequence_numbers(250)
     print_sequence_numbers(500)
     print()
-    print('T ITERATIONS OF N RANDOM NUMBERS')
-    print(f' {GRAY}[OK] N=××× (T=×××) --> AVERAGE [MIN-MAX]{RESET}')
+    print(f'{WHITE}T ITERATIONS OF N RANDOM NUMBERS{RESET}')
+    print(f' {GRAY}[OK] N=### (T=###) --> AVERAGE [  MIN -   MAX]  POINTS   AVG_TIME{RESET}')
     print_random_numbers(6, 80)
-    print_random_numbers(8, 80)
-    print_random_numbers(16, 80)
-    print_random_numbers(32, 60)
-    print_random_numbers(64, 40)
-    print_random_numbers(256, 20)
-    #print_random_numbers(1024, 10)
-    #print_random_numbers(5000, 2)
-    print()
+    print_random_numbers(10, 70)
+    print_random_numbers(15, 60)
+    print_random_numbers(25, 50)
+    print_random_numbers(50, 40)
+    print_random_numbers(80, 30)
     print_random_numbers(100, 100)
+    print_random_numbers(125, 20)
+    print_random_numbers(250, 20)
     print_random_numbers(500, 50)
+    #print_random_numbers(1000, 10)
+    #print_random_numbers(5000, 2)
     print()
 
 def print_input_output_tests():
-    print('INPUT/OUTPUT')
-    print(f'{GRAY}[OK] push_swap(INPUT) = [MOVEMENTS ; ERROR ; RETURN_CODE]{RESET}')
     for numbers in TESTS:
         result, actions, ps_err, ps_code = sort_and_check(numbers)
-        ok = ok_ko_string(result)
-        numbers = [f'{BLUE}{n}{RESET}' if isinstance(n, int) else f'{BLUE}“{n}”{RESET}' for n in numbers]
-        actions = f'{MAGENTA}{actions}{RESET}'
-        ps_err = f'{MAGENTA}{ps_err if ps_err else "✓"}{RESET}'
-        ps_code = f'{MAGENTA}{ps_code}{RESET}'
-        sep = f'{GRAY};{RESET}'
-        print(f'{ok} push_swap({", ".join(numbers)}) = [{actions} {sep} {ps_err} {sep} {ps_code}]')
+        print_push_swap_results(numbers, result, actions, ps_err, ps_code)
 
 def print_all_comb_numbers(length):
     combinations = calc_comb_numbers(length)
-    print(f'  N={BLUE}{length}{RESET}')
     for numbers in combinations:
         result, actions, ps_err, ps_code = sort_and_check(numbers)
-        ok = ok_ko_string(result)
-        numbers = [f'{BLUE}{n}{RESET}' if isinstance(n, int) else f'{BLUE}“{n}”{RESET}' for n in numbers]
-        actions = f'{MAGENTA}{actions}{RESET}'
-        ps_err = f'{MAGENTA}{ps_err if ps_err else "✓"}{RESET}'
-        ps_code = f'{MAGENTA}{ps_code}{RESET}'
-        sep = f'{GRAY};{RESET}'
-        print(f'  {ok} push_swap({", ".join(numbers)}) = [{actions} {sep} {ps_err} {sep} {ps_code}]')
+        print_push_swap_results(numbers, result, actions, ps_err, ps_code, '  ')
 
-def print_comb_numbers(length, rating=True):
-    ok, avgm, minm, maxm = sort_and_stats(calc_comb_numbers(length))
-    rating = calc_rating(length, [avgm, minm, maxm]) if rating else ''
-    print(f'  {ok_ko_string(ok)} N={BLUE}{length}{RESET} --> {MAGENTA}{avgm:.2f}{RESET} [{GREEN}{minm}{RESET}-{RED}{maxm}{RESET}]{rating}')
+def print_comb_numbers(length):
+    ok, avgm, minm, maxm, exec_time = sort_and_stats(calc_comb_numbers(length))
+    points = evaluation_points(length, [avgm, minm, maxm])
+    print(f'  {ok_ko_string(ok)} N={LBLUE}{length}{RESET} --> {CYAN}{avgm:7.2f}{RESET} [{LGREEN}{minm:3d}{RESET} - {LRED}{maxm:3d}{RESET}]  {points}  {BLUE}{exec_time:6.2f}ms{RESET}')
 
-def print_sequence_numbers(length, rating=True):
-    ok, avgm, minm, maxm = sort_and_stats(calc_sequence_numbers(length))
-    rating = calc_rating(length, [avgm, minm, maxm]) if rating else ''
-    print(f' {ok_ko_string(ok)} N={BLUE}{length:3d}{RESET} --> {MAGENTA}{avgm:.1f}{RESET} [{GREEN}{minm}{RESET}-{RED}{maxm}{RESET}]{rating}')
+def print_sequence_numbers(length):
+    ok, avgm, minm, maxm, exec_time = sort_and_stats(calc_sequence_numbers(length))
+    points = evaluation_points(length, [avgm, minm, maxm])
+    print(f' {ok_ko_string(ok)} N={LBLUE}{length:3d}{RESET} --> {CYAN}{avgm:7.1f}{RESET} [{LGREEN}{minm:5d}{RESET} - {LRED}{maxm:5d}{RESET}]  {points}  {BLUE}{exec_time:6.2f}ms{RESET}')
 
-def print_random_numbers(length, count, rating=True):
-    ok, avgm, minm, maxm = sort_and_stats(calc_random_numbers(length, count))
-    rating = calc_rating(length, [avgm, minm, maxm]) if rating else ''
-    print(f' {ok_ko_string(ok)} N={BLUE}{length:3d}{RESET} (T={BLUE}{count:3d}{RESET}) --> {MAGENTA}{avgm:6.1f}{RESET} [{GREEN}{minm:3d}{RESET}-{RED}{maxm:3d}{RESET}]{rating}')
+def print_random_numbers(length, iterations):
+    ok, avgm, minm, maxm, exec_time = sort_and_stats(calc_random_numbers(length, iterations))
+    points = evaluation_points(length, [avgm, minm, maxm])
+    print(f' {ok_ko_string(ok)} N={LBLUE}{length:3d}{RESET} (T={LBLUE}{iterations:3d}{RESET}) --> {CYAN}{avgm:7.1f}{RESET} [{LGREEN}{minm:5d}{RESET} - {LRED}{maxm:5d}{RESET}]  {points}  {BLUE}{exec_time:6.2f}ms{RESET}')
 
 def sort_numbers(numbers: list) -> tuple:
     numbers = [str(n) for n in numbers]
-
     ps_command = [PUSH_SWAP] + numbers
     ps_process = subprocess.run(ps_command, capture_output=True, text=True)
     ps_output = ps_process.stdout
@@ -198,33 +192,18 @@ def sort_numbers(numbers: list) -> tuple:
             action_count += 1
         else:
             ps_error = INVALID_OUTPUT.format(line)
-
-    # print('PUSH-SWAP')
-    # print(f' actions->{"·".join(action_lines)}')
-    # print(f' count--->{action_count}')
-    # print(f' error--->{ps_error}')
-    # print(f' rcode--->{ps_rcode}')
     return action_count, ps_output, ps_error, ps_rcode
 
 def check_actions(numbers: list, actions: str) -> tuple:
     numbers = [str(n) for n in numbers]
-
     chk_command = [CHECKER] + numbers
     chk_process = subprocess.run(chk_command, capture_output=True, text=True, input=actions)
     chk_output = chk_process.stdout.strip()
     chk_error = chk_process.stderr.strip()
     chk_rcode = chk_process.returncode
-
-    # print('CHECKER')
-    # chk_args = ','.join(chk_process.args[1:])
-    # print(f' args---->{chk_args}')
-    # print(f' output-->{chk_output}')
-    # print(f' error--->{chk_error}')
-    # print(f' rcode--->{chk_rcode}')
     return chk_output, chk_error, chk_rcode
 
 def compare_outputs(ps_out, ps_err, chk_out, chk_err) -> bool:
-    # return chk_out == 'OK' or (chk_err == 'Error' and ps_err) or (chk_out == '' and not ps_err and not ps_out.strip())
     return (chk_out == 'OK' and not ps_err) or (chk_err == ps_err and chk_err in ('', 'Error'))
 
 def sort_and_check(numbers: list) -> tuple:
@@ -233,16 +212,11 @@ def sort_and_check(numbers: list) -> tuple:
     result = compare_outputs(ps_out, ps_err, chk_out, chk_err)
     return result, count, ps_err, ps_code
 
-def ok_ko_string(result: bool) -> str:
-    if result:
-        return f'[{GREEN}OK{RESET}]'
-    else:
-        return f'[{RED}KO{RESET}]'
-
 def sort_and_stats(numbers_list: list):
     total_moves = 0
     min_moves = -1
     max_moves = -1
+    start = time.time()
     for numbers in numbers_list:
         result, count, ps_err, ps_code = sort_and_check(numbers)
         if not result:
@@ -252,79 +226,24 @@ def sort_and_stats(numbers_list: list):
             max_moves = count
         if count < min_moves or min_moves < 0:
             min_moves = count
+    exec_time = (time.time() - start) / len(numbers_list) * 1000
     avg_moves = total_moves / len(numbers_list)
-    return result, avg_moves, min_moves, max_moves
+    return result, avg_moves, min_moves, max_moves, exec_time
 
-def points_3(moves):
-    result = []
-    for move in moves:
-        if move <= 3:
-            result.append(1)
-        else:
-            result.append(0)
-    return result
+def print_push_swap_results(numbers: list, result:bool, count: int, ps_err: str, ps_code: int, padding='') -> None:
+    ok = ok_ko_string(result)
+    numbers = [f'{LBLUE}{n}{RESET}' if isinstance(n, int) else f'{LBLUE}“{n}”{RESET}' for n in numbers]
+    count = f'{CYAN}{count}{RESET}'
+    ps_err = f'{CYAN}{ps_err if ps_err else "—"}{RESET}'
+    ps_code = f'{CYAN}{ps_code}{RESET}'
+    sep = f'{GRAY};{RESET}'
+    print(f'{padding}{ok} push_swap({", ".join(numbers)}) = {count} {sep} {ps_err} ({ps_code})')
 
-def points_5(moves):
-    result = []
-    for move in moves:
-        if move <= 12:
-            result.append(1)
-        else:
-            result.append(0)
-    return result
-
-def points_100(moves):
-    result = []
-    for move in moves:
-        if move < 700:
-            result.append(5)
-        elif move < 900:
-            result.append(4)
-        elif move < 1100:
-            result.append(3)
-        elif move < 1300:
-            result.append(2)
-        elif move < 1500:
-            result.append(1)
-        else:
-            result.append(0)
-    return result
-
-def points_500(moves):
-    result = []
-    for move in moves:
-        if move < 5500:
-            result.append(5)
-        elif move < 7000:
-            result.append(4)
-        elif move < 8500:
-            result.append(3)
-        elif move < 10000:
-            result.append(2)
-        elif move < 11500:
-            result.append(1)
-        else:
-            result.append(0)
-    return result
-
-def points_message(points):
-    return f'{YELLOW}→ {BLUE}puntos: {MAGENTA}{points[0]}{GRAY} [{GREEN}{points[1]}{GRAY}-{RED}{points[2]}{GRAY}]{RESET}'
-
-def complex_message(length):
-    complex = math.ceil(math.log(length, 2) * length)
-    return f'{YELLOW}← {BLUE}n·log n{GRAY}={CYAN}{complex}{RESET}'
-
-def rating_message(length, moves):
-    if length == 3:
-        return points_message(points_3(moves))
-    if length == 5:
-        return points_message(points_5(moves))
-    if length == 100:
-        return points_message(points_100(moves))
-    if length == 500:
-        return points_message(points_500(moves))
+def ok_ko_string(result: bool) -> str:
+    if result:
+        return f'[{GREEN}OK{RESET}]'
     else:
-        return complex_message(length)
+        return f'[{RED}KO{RESET}]'
 
 def calc_comb_numbers(length):
     numbers = list(range(1, length + 1))
@@ -360,12 +279,73 @@ def calc_random_numbers(length, count):
         count -= 1
     return result
 
-def calc_rating(length, moves):
-    if moves[1] < 0:
-        result = f'  [{RED}KO{RESET}]'
-    else:
-        result = f'  {rating_message(length, moves)}'
-    return result
+def evaluation_points(length, stats):
+    points = {
+        3: points_3(stats),
+        5: points_5(stats),
+        100: points_100(stats),
+        500: points_500(stats),
+    }
+    return points.get(length, f'{"":7}')
+
+def points_3(stats):
+    result = []
+    for value in stats:
+        if value <= 3:
+            result.append(1)
+        else:
+            result.append(0)
+    return points_message(result)
+
+def points_5(stats):
+    result = []
+    for value in stats:
+        if value <= 12:
+            result.append(1)
+        else:
+            result.append(0)
+    return points_message(result)
+
+def points_100(stats):
+    result = []
+    for value in stats:
+        if value < 700:
+            result.append(5)
+        elif value < 900:
+            result.append(4)
+        elif value < 1100:
+            result.append(3)
+        elif value < 1300:
+            result.append(2)
+        elif value < 1500:
+            result.append(1)
+        else:
+            result.append(0)
+    return points_message(result)
+
+def points_500(stats):
+    result = []
+    for value in stats:
+        if value < 5500:
+            result.append(5)
+        elif value < 7000:
+            result.append(4)
+        elif value < 8500:
+            result.append(3)
+        elif value < 10000:
+            result.append(2)
+        elif value < 11500:
+            result.append(1)
+        else:
+            result.append(0)
+    return points_message(result)
+
+def points_message(points):
+    return f'{CYAN}{points[0]}{RESET} [{LGREEN}{points[1]}{RESET}-{LRED}{points[2]}{RESET}]'
+
+# def complexity_estimation(length):
+#     complex = math.ceil(math.log(length, 2) * length)
+#     return f'{YELLOW}← {BLUE}n·log n{GRAY}={CYAN}{complex}{RESET}'
 
 if __name__ == "__main__":
     main()
